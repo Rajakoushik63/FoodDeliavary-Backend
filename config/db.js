b069b5d07490de853be275686c273aa0195e9dbf
rajak <rajakoushik8008@gmail.com>
2025-02-04 21:46:40 +0530

    update .env file

--- config/db.js
+++ config/db.js
@@ -1,28 +1,9 @@
-// import mogoose from "mongoose";
+import mogoose from "mongoose";
 
-// export const connectDB = async () => {
-//   await mogoose
-//     .connect(
-//       "mongodb+srv://rajakoushik8008:ogg4SW66r4JzeJei@cluster0.zmmk3.mongodb.net/foodDelivary-Backend"
-//     )
-//     .then(() => console.log("DB Connected"));
-// };
-
-import mongoose from "mongoose";
-import dotenv from "dotenv";
-
-// Load environment variables
-dotenv.config();
-
-const connectDB = async () => {
-  try {
-    // console.log("MongoDB URI:", process.env.MONGODB_URI); // Check the URI
-    await mongoose.connect(process.env.MONGODB_URI);
-    console.log("Connected to MongoDB");
-  } catch (err) {
-    console.error("Could not connect to MongoDB", err);
-    process.exit(1);
-  }
+export const connectDB = async () => {
+  await mogoose
+    .connect(
+      "mongodb+srv://rajakoushik8008:ogg4SW66r4JzeJei@cluster0.zmmk3.mongodb.net/foodDelivary-Backend"
+    )
+    .then(() => console.log("DB Connected"));
 };
-
-export { connectDB };
